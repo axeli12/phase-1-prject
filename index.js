38bb5f6d6fe4d0cf7039961a7ba88f5eb626014f
Axel irias <axelirias@rocketmail.com>
2022-07-05 22:09:27 -0600

    edit js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita`) 
+function getDrinksName() {
+    
+    const searchText = document.querySelector('.searchText').ariaValue
+
+    fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`)
+    .then (response => response.json())
+    .then(data => {
+        console.log(data.drinks)
+    })
+}
+
+
+
