3ad0911c9647354c65d3942ea88114033306a2f0
Axel irias <axelirias@rocketmail.com>
2022-07-06 18:45:54 -0600

    added

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-function getDrinksName() {
+document.addEventListener('DOMContentLoaded',()=>{
     
-    const searchText = document.querySelector('.searchText').ariaValue
+    startCountdown()
+})
 
-    fetch(`www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`)
-    .then (response => response.json())
-    .then(data => {
-        console.log(data.drinks)
-    })
+function pagerefresh(){
+    main.innerHTML = ''
 }
 
+function getDrinkName() {
 
+    const searchText = document.querySelector('.searchText').value;
+
+    fetch (`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchText}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data.drinks);
+
+
+        if(data.drinks == null){
+            alert("Drink not found.")
+            return;
+        }else{
+            let chosenDrink = document.querySelector('#drinkThumbnail')
+            chosenDrink.src = data.drinks[0].strDrinkThumb
+
+            let chosenDrinkName = document.querySelector('.drinkName');
+            chosenDrinkName.innerHTML = data.drinks[0].strDrink;
+
+          
+            let chosenDrinkInstructions = document.querySelector('.drinkInstructions');
+            chosenDrinkInstructions.innerHTML = data.drinks[0].strInstructions.split(' ').join('');
+
+        
+            let chosenDrinkIngredients = document.querySelector('.drinkIngredients');
+
+            for (let i = 1; i < 15; i++) {
+                if (data.drinks[0][`strIngredient${i}`] != null && data.drinks[0][`strMeasure${i}`] != null) {
+                    chosenDrinkIngredients.innerHTML += `<li> ${data.drinks[0][`strMeasure${i}`]} ${data.drinks[0][`strIngredient${i}`]} </li>`;
+                }
+        }
+    };
+})
+.catch(error => alert(error))
+
+}
+ 
+ document.querySelector('#modalDisplay').style.display = 'block';
+
+
+
+
+function closeButton() {
+    document.querySelector('#modalDisplay').style.display = 'none';
+}
+
+document.getElementById('searchButton').addEventListener('click', getDrinkName);
+document.getElementById('closeButton').addEventListener('click', closeButton);
 
